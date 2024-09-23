ad6af80d05d0e3a01d3bbfb8ef837c6d791a8b82
mohit kala jain <mohitkala0@gmail.com>
2024-09-23 20:29:48 +0530

    Add project files.

---
+++ CustomMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace MiddlewareExample
+{
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public CustomMiddleware(RequestDelegate next)
+        {
+                _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //Custome Logic
+            Console.WriteLine("Custom Middleware : Request handling");
+
+            if (!context.Request.Path.Value.Contains("/blocked"))
+            {
+                await _next(context);
+            }
+            else
+            {
+                context.Response.StatusCode=StatusCodes.Status403Forbidden;
+                await context.Response.WriteAsync("This Request Blocked by Middleware");
+            }
+
+            Console.WriteLine("Custom Middleware : Request Finished");
+        }
+    }
+}
